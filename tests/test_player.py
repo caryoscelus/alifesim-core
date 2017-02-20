ac14f66ee446a3a169e432dacc9e16d3aaa8cd70
caryoscelus <caryoscelus@gmx.com>
2017-02-20 12:25:35 +0300

    Entity, basic stats, player

---
+++ tests/test_player.py
@@ -0,0 +1,8 @@
+"""Very basic tests like creating Player instance
+"""
+
+from alifesim.player import *
+from alifesim import name, money, basic_stats
+
+def test_new_player():
+    player = Player()
