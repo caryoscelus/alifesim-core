d0c38c806e6fbbbd592f9d53439bdcb0e09d92cb
caryoscelus <caryoscelus@gmx.com>
2017-02-20 17:06:48 +0300

    Playground

---
+++ alifesim/playground.py
@@ -0,0 +1,33 @@
+##
+##  Copyright (C) 2017 caryoscelus
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""Playground - test-related code
+"""
+
+from .person import Person
+from .friends import make_friends
+
+def setup_friends(player):
+    player.name = 'Mee Mines'
+    friend_names = [
+        'Frie Ends',
+        'Clou Sefri',
+    ]
+    for name in friend_names:
+        friend = Person()
+        friend.name = name
+        make_friends(player, friend)
