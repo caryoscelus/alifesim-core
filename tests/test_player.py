8af65c5d62629770f3f42474f493b44f452b5cf7
caryoscelus <caryoscelus@gmx.com>
2017-02-20 12:28:05 +0300

    Another player test

--- tests/test_player.py
+++ tests/test_player.py
@@ -6,3 +6,9 @@ from alifesim import name, money, basic_stats
 
 def test_new_player():
     player = Player()
+
+def test_player_stats():
+    player = Player()
+    assert player.money == 0
+    assert player.name == ''
+    assert player.iq == 0
